75b08727f5f6487e4d717d2a40eb88664f2ccdb1
abusidi7410 <abdurrahmansidiabuba2@gmail.com>
2026-07-27 03:42:06 -0700

    Fix corrupted entrypoint script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,1 +1,10 @@
-﻿#!/bin/shset -ephp artisan migrate --forcephp artisan config:clear || truephp artisan cache:clear || trueexec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+﻿#!/bin/sh
+
+set -e
+
+php artisan migrate --force
+
+php artisan config:clear || true
+php artisan cache:clear || true
+
+exec php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
